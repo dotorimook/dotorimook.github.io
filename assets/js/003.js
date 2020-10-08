5524e445538cb8507c4bf6c66436af2d1a32b232
yjh0719 <yjh0719@midasit.com>
2020-10-08 13:59:06 +0900

    Chore: remove onload

--- assets/js/003.js
+++ assets/js/003.js
@@ -1,30 +1,28 @@
-document.body.addEventListener('load', () => {
-  const draggables = document.querySelectorAll('*[draggable=true]');
-  draggables.forEach((draggable) => {
-    draggable.style.position = 'fixed';
-    const screenW = document.body.clientWidth;
-    const screenH = document.body.clientHeight;
-    const windowRect = draggable.getBoundingClientRect();
-    const top = Math.random() * screenW - windowRect.width;
-    const left = Math.random() * screenH - windowRect.height;
-    draggable.style.left = `${top > 0 ? top : 0}px`;
-    draggable.style.top = `${left > 0 ? left: 0}px`;
-  });
-  let initOffsetX = 0, initOffsetY = 0;
-  document.body.addEventListener('dragstart', (e) => {
-    if(e.target.classList.contains('draggable')) {
-      initOffsetX = e.offsetX;
-      initOffsetY = e.offsetY;
-      e.dataTransfer.setDragImage(document.createElement('img'),0,0);
+const draggables = document.querySelectorAll('*[draggable=true]');
+draggables.forEach((draggable) => {
+  draggable.style.position = 'fixed';
+  const screenW = document.body.clientWidth;
+  const screenH = document.body.clientHeight;
+  const windowRect = draggable.getBoundingClientRect();
+  const top = Math.random() * screenW - windowRect.width;
+  const left = Math.random() * screenH - windowRect.height;
+  draggable.style.left = `${top > 0 ? top : 0}px`;
+  draggable.style.top = `${left > 0 ? left: 0}px`;
+});
+let initOffsetX = 0, initOffsetY = 0;
+document.body.addEventListener('dragstart', (e) => {
+  if(e.target.classList.contains('draggable')) {
+    initOffsetX = e.offsetX;
+    initOffsetY = e.offsetY;
+    e.dataTransfer.setDragImage(document.createElement('img'),0,0);
+  }
+});
+document.body.addEventListener('drag', (e) => {
+  if(e.target.classList.contains('draggable')) {
+    const rect = e.target.getBoundingClientRect();
+    if(!!e.screenX && !!e.screenY) {
+      e.target.style.left = `${rect.x + e.offsetX - initOffsetX}px`;
+      e.target.style.top = `${rect.y + e.offsetY - initOffsetY}px`;
     }
-  });
-  document.body.addEventListener('drag', (e) => {
-    if(e.target.classList.contains('draggable')) {
-      const rect = e.target.getBoundingClientRect();
-      if(!!e.screenX && !!e.screenY) {
-        e.target.style.left = `${rect.x + e.offsetX - initOffsetX}px`;
-        e.target.style.top = `${rect.y + e.offsetY - initOffsetY}px`;
-      }
-    }
-  });
+  }
 });
