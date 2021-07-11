7308ee5d5d545b308e35fea2a79ab5975991e0dd
yjh0719 <yjh0719@midasit.com>
2021-07-11 15:56:55 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-841ea697faa4c6556fb3.js"
+    "url": "webpack-runtime-679c4e5f53200a30e5d1.js"
   },
   {
     "url": "styles.e4341d76e25e5efb0870.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bce7bf7cc3d456b837ae333f3f761834"
+    "revision": "fb9f205518fa39c5d0ad19edfadb4856"
   },
   {
     "url": "polyfill-d4b9477c183dc5c8fd29.js"
