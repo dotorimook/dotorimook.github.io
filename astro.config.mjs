40a9da8b32f4d896f401d54833c6d3a7f6ba0deb
blair.berry <blair.berry@kakaomobility.com>
2026-02-09 00:21:28 +0900

    fix: exclude @emotion/is-prop-valid from vite optimization to resolve CI error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,6 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dotorimook.github.io',
@@ -20,13 +12,11 @@ export default defineConfig({
     sitemap()
   ],
   vite: {
-    resolve: {
-      alias: {
-        '@emotion/is-prop-valid': path.resolve(__dirname, 'node_modules/@emotion/is-prop-valid/dist/emotion-is-prop-valid.cjs.js'),
-      },
-    },
     ssr: {
-      noExternal: ['@emotion/styled', '@emotion/react', '@emotion/is-prop-valid'],
+      noExternal: ['@emotion/styled', '@emotion/react'],
+    },
+    optimizeDeps: {
+      exclude: ['@emotion/is-prop-valid'],
     },
   },
 });
